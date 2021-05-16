9cdbb333480333e67ff154efda3228db6919d45c
Jack Clarke <jackclarke95@gmail.com>
2021-05-16 23:16:01 +0100

    find random encounter from list

--- src/Components/TestEncounters.tsx
+++ src/Components/TestEncounters.tsx
@@ -3,8 +3,7 @@ import { firebaseDb } from "../firebase.utils";
 
 const TestEncounters = () => {
   var [encounters, setEncounters] = useState({});
-  var [currentId, setCurrentId] = useState("");
-  var [currentObject, setCurrentObject] = useState({});
+  var [currentEncounter, setCurrentEncounter] = useState({} as any);
 
   useEffect(() => {
     firebaseDb.child("encounters").on("value", (snapshot) => {
@@ -22,27 +21,72 @@ const TestEncounters = () => {
 
   const addTestEncounter = () => {
     const encounters = [
-      { arctic: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { coastal: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { desert: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { forest: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { grassland: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { hill: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { mountain: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { swamp: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { underdark: [{ test: "encounter 1" }, { name: "encounter 2" }] },
-      { urban: [{ test: "encounter 1" }, { name: "encounter 2" }] },
+      { terrain: "arctic", enemies: [{ type: "giant owl", quantity: 1 }] },
+      {
+        terrain: "arctic",
+        discovery:
+          "A partially eaten carcass of a mammoth, from which 1d4 rations can be harvested",
+      },
+      {
+        terrain: "arctic",
+        enemies: [
+          {
+            type: "kobold",
+            quantity: { die: 6, "die-count": 1, modifier: 3 },
+          },
+        ],
+      },
+      {
+        terrain: "arctic",
+        enemies: [
+          {
+            type: "bandit",
+            quantity: { die: 6, "die-count": 2 },
+          },
+          { type: "bandit captain", quantity: 1 },
+        ],
+      },
     ];
 
     var randomEncounter =
       encounters[Math.floor(Math.random() * encounters.length)];
+    randomEncounter.enemies?.map((monster) => {});
+
     addEncounter(randomEncounter);
+  };
+
+  const getRandomEncounter = () => {
+    let recordCount;
+
+    // firebaseDb.child("encounters").once("value", (snapshot) => {
+    //   recordCount = snapshot.numChildren();
+    // });
+
+    let array = [] as any;
+
+    firebaseDb.child("encounters").once("value", (snapshot) => {
+      snapshot.forEach((item) => {
+        recordCount = snapshot.numChildren();
+        array.push(item.val());
+      });
+    });
+
+    const randomIndex = Math.floor(Math.random() * recordCount);
+
+    const encounter = array[randomIndex];
+
+    console.log(encounter);
+
+    setCurrentEncounter(encounter);
+
+    return encounter;
   };
 
   return (
     <>
       <h2>Encounters</h2>
       <button onClick={() => addTestEncounter()}>Add Random Encounter</button>
+      <button onClick={() => getRandomEncounter()}>Log Random Encounter</button>
       <table
         style={{
           borderCollapse: "collapse",
@@ -63,17 +107,21 @@ const TestEncounters = () => {
                 key={key}
                 data-id={encounter.id}
               >
-                <td>{encounter.name}</td>
+                <td>{encounter.terrain}</td>
                 <td>
-                  {encounter["dungeon-master"]
-                    ? encounter["dungeon-master"]
-                    : "None yet"}
+                  {encounter.discovery
+                    ? encounter.discovery
+                    : encounter.enemies.map((enemy) => {
+                        return enemy.type;
+                      })}
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      <div>{`Terrain: ${currentEncounter}`}</div>
+      {/* <div>{`Terrain: ${currentEncounter}`}</div> */}
     </>
   );
 };
