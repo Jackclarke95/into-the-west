76dbae505cacfb5cf08954e7de096ac12e2238dd
Jack Clarke <jackclarke95@gmail.com>
2021-05-20 23:55:29 +0100

    send session to discord

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { BrowserView, MobileView } from "react-device-detect";
-import { Webhook, MessageBuilder } from "discord-webhook-node";
 import Character from "./Components/CharacterCard.";
 import TestSessions from "./Components/TestSessions";
 import TestEncounters from "./Components/TestEncounters";
@@ -18,63 +17,6 @@ function App() {
   const [sessions, setSessions] = useState([] as {}[]);
   const [user, setUser] = useState({} as any);
   const [currentPlayer, setCurrentPlayer] = useState();
-
-  const sendDiscordMessage = () => {
-    let imageUrl;
-
-    firestore
-      .ref(`Avatars/Eslyn Juhlenath.jpeg`)
-      .getDownloadURL()
-      .then((url) => {
-        console.log("test");
-        console.log(url);
-        imageUrl = url;
-      })
-      .catch((e) => {
-        console.log("borked");
-        imageUrl =
-          "https://www.dndbeyond.com/Content/Skins/Waterdeep/images/characters/default-avatar-builder.png";
-      });
-
-    console.log(imageUrl);
-
-    const hook = new Webhook(
-      "https://discord.com/api/webhooks/845060074015555605/T7uXBkLL8PYI4jgKc-yZU99tIem99BYN8OYo21qY4sLqeZEvChzG3wHMYlAOIB3Do7WE"
-    );
-
-    const embed = new MessageBuilder()
-      .setThumbnail(imageUrl)
-      .setImage(imageUrl)
-      .setTitle("Eslyn has suggested a new adventure!")
-      .setDescription("Hunt for the Thessalhydra")
-      .setFooter("Please see the website for more details")
-      .addField("field1", "field1 desc")
-      //.setColor(255255255)
-      .addField("field2", "field2 desc")
-      .setAuthor("Adventure Announcer")
-      .setTimestamp();
-
-    let foo = hook.send(embed);
-
-    console.log("foo", foo);
-
-    // var request = new XMLHttpRequest();
-
-    // request.open(
-    //   "POST",
-    //   "https://discord.com/api/webhooks/845060074015555605/T7uXBkLL8PYI4jgKc-yZU99tIem99BYN8OYo21qY4sLqeZEvChzG3wHMYlAOIB3Do7WE"
-    // );
-
-    // var params = {
-    //   username: "Spidey Bot",
-    //   avatar_url: "",
-    //   content: "test message",
-    // };
-
-    // console.log("Sending message", `${params.username}; "${params.content}"`);
-
-    // request.send(JSON.stringify(params));
-  };
 
   auth.onAuthStateChanged((user) => {
     setUser(user);
@@ -99,6 +41,7 @@ function App() {
       .orderByChild("name")
       .once("value", (snapshot) => {
         snapshot.forEach((child) => {
+          console.log(child.val().name);
           characterArray.push(child.val());
         });
         setCharacters(characterArray);
@@ -133,7 +76,6 @@ function App() {
           </MobileView>
         </>
       )}
-      <button onClick={sendDiscordMessage}>Send Discord Message</button>
       <h1>Into The West</h1>
       <div className="characters" style={{ textAlign: "center" }}>
         <div className="character-cards">
--- src/Components/TestSessions.tsx
+++ src/Components/TestSessions.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Webhook, MessageBuilder } from "discord-webhook-node";
 import { firebaseDb } from "../firebase.utils";
 import { determineSessionCharacters } from "../Helpers/DataHelper";
 
@@ -26,6 +27,26 @@ const TestSessions = ({ characters, sessions }) => {
 
   const addNewSession = () => {
     console.log(currentSession);
+  };
+
+  const sendDiscordMessage = (session) => {
+    const hook = new Webhook(
+      "https://discord.com/api/webhooks/845060074015555605/T7uXBkLL8PYI4jgKc-yZU99tIem99BYN8OYo21qY4sLqeZEvChzG3wHMYlAOIB3Do7WE"
+    );
+
+    const embed = new MessageBuilder()
+      .setAuthor("Adventure Announcer")
+      .setTitle(
+        session["suggested-by"]
+          ? `${session["suggested-by"]} has suggested a new adventure!`
+          : "A new adventure has been suggested!"
+      )
+      .setDescription(session.name)
+      .addField("Dungeon Master", session["dungeon-master"] ?? "N/A")
+      .setFooter("Please see the website for more details")
+      .setTimestamp();
+
+    hook.send(embed);
   };
 
   return (
@@ -113,6 +134,9 @@ const TestSessions = ({ characters, sessions }) => {
                     }}
                   >
                     Delete
+                  </button>
+                  <button onClick={() => sendDiscordMessage(session)}>
+                    Send To Discord
                   </button>
                 </td>
               </tr>
