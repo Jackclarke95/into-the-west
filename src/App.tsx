77521b62eed19f90b7456ce6e3345152ff35179f
Jack Clarke <jackclarke95@gmail.com>
2022-08-01 22:46:37 +0100

    define `env:`

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,8 @@ import MobileHeader from "./Components/MobileHeader";
 import MobileDashboard from "./Components/MobileDashboard";
 import MobileFooter from "./Components/MobileFooter";
 
+console.log(process.env);
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
