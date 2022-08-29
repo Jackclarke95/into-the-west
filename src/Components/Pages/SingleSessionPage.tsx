d8758869834de2895d4e6cf8c4b2f7003dfca286
Jack Clarke <jackclarke95@gmail.com>
2022-08-29 23:53:29 +0100

    Session page hide/show register buttons

--- src/Components/Pages/SingleSessionPage.tsx
+++ src/Components/Pages/SingleSessionPage.tsx
@@ -1,6 +1,7 @@
 import { DefaultButton, PrimaryButton, Stack } from "@fluentui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import DataService from "../../Helpers/DataService";
 import BasePage from "./BasePage";
 
 const SingleSessionPage = () => {
@@ -8,15 +9,52 @@ const SingleSessionPage = () => {
   const { sessionId } = useParams();
 
   const sessions = useSelector((state) => state.sessions);
+  const currentPlayer = useSelector((state) => state.currentPlayer);
+  const sessionInterests = useSelector((state) => state.sessionInterests);
 
   const session = sessions.isLoading
     ? null
     : sessions.data.find((session) => session.id === sessionId);
 
+  const playerInterestInSession =
+    sessionInterests.isLoading || currentPlayer.isLoading || !session
+      ? undefined
+      : sessionInterests.data.find(
+          (interest) =>
+            interest.playerId === currentPlayer.data?.id &&
+            interest.sessionId === session.id
+        );
+
+  const playerIsInterested = !!playerInterestInSession;
+
   const onClickSignUp = () => {
     dispatch({
       type: "SetSessionRegistration",
       sessionRegistration: { isShown: true, session: session! },
+    });
+  };
+
+  const onClickSetDate = () => {
+    console.log("Clicked set date");
+  };
+
+  const onClickCompleteSession = () => {
+    console.log("Clicked complete session");
+  };
+
+  const onClickRemoveFromSession = async () => {
+    if (!playerInterestInSession) {
+      return;
+    }
+
+    dispatch({
+      type: "SetConfirmation",
+      confirmation: {
+        isShown: true,
+        message: "Are you sure you wish to unregister from this session?",
+        onConfirm: async () =>
+          await DataService.unregisterFromSession(playerInterestInSession),
+      },
     });
   };
 
@@ -28,9 +66,17 @@ const SingleSessionPage = () => {
         styles={{ root: { padding: 10 } }}
         tokens={{ childrenGap: 10 }}
       >
-        <PrimaryButton onClick={onClickSignUp}>Sign up</PrimaryButton>
-        <DefaultButton onClick={onClickSignUp}>Set date</DefaultButton>
-        <DefaultButton onClick={onClickSignUp}>Complete session</DefaultButton>
+        {!playerIsInterested && (
+          <PrimaryButton text="Sign Up" onClick={onClickSignUp} />
+        )}
+        {playerIsInterested && (
+          <PrimaryButton text="Unregister" onClick={onClickRemoveFromSession} />
+        )}
+        <DefaultButton text="Set date" onClick={onClickSetDate} />
+        <DefaultButton
+          text="Complete session"
+          onClick={onClickCompleteSession}
+        />
       </Stack>
     </BasePage>
   );
